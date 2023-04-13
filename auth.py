c6e80dfd2a076fdd9ff1bd54701db9df4053b9cc
Kori Njoroge <korinjoroge63@gmail.com>
2023-04-13 19:42:43 +0300

    route for login

--- auth.py
+++ auth.py
@@ -1,16 +1,26 @@
-from flask import Blueprint, render_template,url_for
+from flask import Blueprint, render_template, url_for, request, redirect
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-
-@auth.route('/login')
+@auth.route('/login', methods=['POST', 'GET'])
 def login():
     return render_template('login.html')
+
+@auth.route('/login-now', methods=['POST', 'GET'])
+def login_post():
+    # login code
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    response = ''
+
+    if username == 'admin' and password == '@admin':
+        response = redirect(url_for('main.index'))
+        # return render_template('login.html')
+    else:
+        response = redirect((url_for('main.home')))
+    return response
 
 
 @auth.route('/logout')
