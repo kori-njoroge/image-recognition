bc860b9f6fd5e56b31f2281edfcda25f115f2367
Kori Njoroge <korinjoroge63@gmail.com>
2023-04-24 00:25:33 +0300

    test file for db connection

--- test.py
+++ test.py
@@ -1,14 +1,29 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-# Replace the placeholders with your actual database credentials
-username = 'sa'
-password = 'MyPass@1'
-server = 'localhost,1433'
-dbname = 'imageDB'
+# create the database engine
+db_string = "mysql+pymysql://root:MyPass@localhost:3306/imageDB"
+engine = create_engine(db_string)
 
-engine = create_engine(f'mssql+pymssql://{username}:{password}@{server}/{dbname}')
+# create the session
+Session = sessionmaker(bind=engine)
+session = Session()
 
-# Test the connection by executing a simple query
-with engine.connect() as connection:
-    result = connection.execute('SELECT 1')
-    print(result.fetchone())
+# create the declarative base
+Base = declarative_base()
+
+# define a class for the products table
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    barcode = Column(String)
+    description = Column(String)
+
+# query the database for a product with a specific barcode
+barcode = '6161101861168'
+product = session.query(Product).filter_by(barcode=barcode).first()
+
+# print the description of the product
+print(product.description)
