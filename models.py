2820b26cf95168f94b14d4232db8398c3e797af2
Kori Njoroge <korinjoroge63@gmail.com>
2023-04-23 20:14:17 +0300

    product db schema

---
+++ models.py
@@ -0,0 +1,15 @@
+from . import db
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    img = db.Column(db.String(500), nullable=False)
+    barcode = db.Column(db.Integer(), nullable=False)
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
