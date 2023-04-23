88e23f99bfbe8aff3819d88db1a107d94a8ab395
Kori Njoroge <korinjoroge63@gmail.com>
2023-04-23 20:14:43 +0300

    db configurations file

--- config.py
+++ config.py
@@ -1,24 +1,32 @@
 import os
+from dotenv import load_dotenv
 
+
+# load environmental variables
+load_dotenv()
+
+# Database configuration
+DB_CONFIG = {
+    'server': os.environ.get('DB_SERVER'),
+    'database': os.environ.get('DB_NAME'),
+    'username': os.environ.get('DB_USERNAME'),
+    'password': os.environ.get('DB_PASSWORD'),
+}
+
+# Secret key for sessions and CSRF protection
+SECRET_KEY = os.environ.get('SECRET_KEY', os.environ.get('SECRET_KEY'))
+
+# Debug mode
+DEBUG = True
+
+# Application root URL
+APPLICATION_ROOT = '/'
+
+# Config settings
 class Config:
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'secret_key' # sets a secret key for secure data transmission
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mssql+pymssql://sa:MyPass@01@127.0.0.1:1433/images'
+    SECRET_KEY = SECRET_KEY
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-class TestingConfig(Config):
-    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "mssql+pymssql://sa:Kheriac@1@localhost/imageDB"
