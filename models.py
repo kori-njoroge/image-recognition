b28be8c16a0ccbd60a85bc5d233adce879012b31
Kori Njoroge <korinjoroge63@gmail.com>
2023-04-24 00:24:58 +0300

    products model

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Product(db.Model):
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(500), nullable=True)
     price = db.Column(db.Float, nullable=False)
-    img = db.Column(db.String(500), nullable=False)
+    image_url = db.Column(db.String(500), nullable=False)
     barcode = db.Column(db.Integer(), nullable=False)
 
     def __repr__(self):
